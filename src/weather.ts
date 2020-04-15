50a7437f3a270b4290b4ab90f5a77807df927875
Allan Hortle <allanhortle@gmail.com>
2020-04-16 08:31:16 +1000

    Colorize wind and rain, dont show null rains

--- src/weather.ts
+++ src/weather.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {observations, forecast} from './data/bom';
 import scale from './data/scale';
-import {underline, bold, blue, green, yellow, red, bgWhite, magenta} from 'chalk';
+import {underline, bold, blue, green, yellow, red, bgWhite, magenta, white, cyan} from 'chalk';
 import List from 'list-it';
 import {format, isSame, now} from 'stringdate';
 import get from 'unmutable/get';
@@ -12,6 +12,7 @@ type Args = {
 };
 
 type Rain = {chance: number, amount: {min: number, max: number | null, units: string}};
+type Wind = {speed_kilometre: number, direction: string};
 
 type HourlyForecast = {
     rain: Rain,
@@ -43,6 +44,8 @@ const tempColor = (num: number, other?: any) => {
 }
 const rainAmount = ({amount}: Rain) => {
     const {min, max} = amount;
+    //return `${min},${max || 0}`;
+    if(!max && !min) return '';
     if(!max) return `${min}mm`;
     if(!min) return `${max}mm`;
     return `${min}-${max}mm`;
@@ -55,6 +58,21 @@ const icon = (ii: string): string => {
     return ii;
 }
 
+const printRain = (rain: Rain) => {
+    const {chance, amount: {min, max}} = rain;
+    let color = chance >= 25 ? chance >= 50 ? blue : cyan : white;
+    let value = `${min}-${max}mm`;
+    if(!max) value = `${min}mm`;
+    if(!min) value = `${max}mm`;
+    if(!max && !min) value = '';
+    return color(`${chance}% ${value}`);
+}
+
+const printWind = ({speed_kilometre, direction}: Wind): string => {
+    const color = speed_kilometre >= 50 ? red : white;
+    return color(`${speed_kilometre}km/h ${direction}`);
+}
+
 function todayTable(data: Array<HourlyForecast>): string {
     const max = Math.max(...data.map(_ => _.temp));
     const min = Math.min(...data.map(_ => _.temp));
@@ -64,14 +82,15 @@ function todayTable(data: Array<HourlyForecast>): string {
     return new List({autoAlign : true, headerUnderline: true})
         .setHeaderRow(['Time', '', 'Temp', '', 'Rain', 'Wind'])
         .d(data
-            .filter(pipe(get('time'), isSame(now(), 'day')))
+            //.filter(pipe(get('time'), isSame(now(), 'day')))
+            .slice(0, 8)
             .map(({rain, time, temp, icon_descriptor, wind}) => [
                 `${format('EEE')(time)} ${format('ha')(time).toLowerCase()}`,
                 icon(icon_descriptor),
                 bar.map((_, index) => index <= scaleBar(temp) ? tempColor(temp, '=') : ' ').join(''),
                 tempColor(temp),
-                `${rain.chance}% ${rainAmount(rain)}`,
-                `${wind.speed_kilometre}km/h ${wind.direction}`
+                printRain(rain),
+                printWind(wind)
             ])
         )
        .toString();
@@ -94,7 +113,7 @@ function forecastTable(data: Array<DailyForecast>): string {
                     return offset >= temp_min && offset <= temp_max ? tempColor(offset, '=') : ' '
                 }).join(''),
                 tempColor(temp_max),
-                `${rain.chance}% ${rainAmount(rain)}`
+                printRain(rain)
             ];
         }))
         .toString();
@@ -111,10 +130,10 @@ export default async function weather(id) {
     const stats = new List({autoAlign: true}).d([
         [`${today.now.now_label}:`, `${celcius(today.now.temp_now)}`],
         [`${today.now.later_label}:`, `${celcius(today.now.temp_later)}`],
-        [`Temperature:`, `${celcius(temp)} (${celcius(temp_feels_like)})`],
-        [`Humidity:`, `${humidity}%`],
+        [`Temp:`, `${celcius(temp)} ${temp_feels_like ? `(${celcius(temp_feels_like)})` : ''}`],
+        [`Humidity:`, `${humidity || 0}%`],
         [`Rain:`, `${rain_since9am || 0}mm`],
-        [`Wind:`, `${wind.speed_kilometre}km/h ${wind.direction}`],
+        [`Wind:`, printWind(wind)]
     ]).toString();
 
 
